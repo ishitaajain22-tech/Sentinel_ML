0e6b6bc6207fe59ee3c23679757f365ae8957d43
Ishita <ishitaajain22@gmail.com>
2026-05-19 02:21:14 +0530

    fix: add vercel

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://sentinel-frontend-three.vercel.app/",
+        "https://sentinel-frontend-three.vercel.app",
         "https://*.vercel.app",
     ],
     allow_credentials=True,
